39f4821a9275998ec3407e7bc92bea53c89b8aa7
Roharui <jack555586@gmail.com>
2022-08-16 14:48:49 +0900

    bug: AWS Api Gateway 버그 수정

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,18 @@ async function bootstrap() {
   let doc: OpenAPIObject;
 
   if (process.env.NODE_ENV === 'dev') {
-    const config = new DocumentBuilder()
+    const _config = new DocumentBuilder()
       .setTitle('FanFixiv 업로드 Swagger')
       .setDescription('Fanfixiv 인증 RestApi Swagger입니다.')
       .setVersion('1.0')
-      .addTag('temp')
-      .build();
+      .addTag('temp');
+
+    if (process.env.DEV_SERVER) {
+      _config.addServer('/upload');
+    }
+
+    const config = _config.build();
     doc = SwaggerModule.createDocument(app, config);
-  }
-  if (process.env.DEV_SERVER) {
-    Object.keys(doc.paths).forEach(function (key) {
-      const newkey = '/upload' + key;
-      doc.paths[newkey] = doc.paths[key];
-      delete doc.paths[key];
-    });
   }
   SwaggerModule.setup('api', app, doc);
 
