334388410d39875f5fefc2c4396ca859e5ee0c6e
Roharui <jack555586@gmail.com>
2022-09-07 22:16:38 +0900

    feat: GET /content API 생성
    Appmodule 에서 ContentModule를 Import 에 추가

---
+++ src/content/content.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Content } from '@src/db/entities/content.entity';
+import { ContentController } from './content.controller';
+import { ContentService } from './content.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Content])],
+  controllers: [ContentController],
+  providers: [ContentService],
+})
+export class ContentModule {}
