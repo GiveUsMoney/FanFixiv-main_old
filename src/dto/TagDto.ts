e548f6f4e4e1fe54b02f772537bd1b58606f326f
Roharui <jack555586@gmail.com>
2022-09-07 22:14:59 +0900

    refactor: response DTO 추가 후 일괄 적용
    Base 엔티티에 있던 Exclude 제거
    엔티티에 class-validator 추가
    tag.controller에서 response 적용

--- src/dto/TagDto.ts
+++ src/dto/TagDto.ts
@@ -1,4 +1,7 @@
-import { IsInt, IsString, IsOptional } from 'class-validator';
+import { Tag } from '@src/db/entities/tag.entity';
+import { ITag, TagTypes } from '@src/interfaces/tag.interface';
+import { Exclude, Expose } from 'class-transformer';
+import { IsInt, IsString, IsOptional, IsEnum } from 'class-validator';
 
 export class LimitDto {
   @IsOptional()
@@ -10,4 +13,40 @@ export class TagDto extends LimitDto {
   s: string;
 
   limit = 5;
+}
+
+@Exclude()
+export class TagResultDto implements ITag {
+  constructor(tag: Tag) {
+    Object.assign(this, tag);
+  }
+
+  @Expose()
+  @IsEnum(TagTypes)
+  type: TagTypes;
+
+  @Expose()
+  @IsString()
+  name: string;
+
+  describe: string;
+
+  using: boolean;
+}
+
+@Exclude()
+export class TagDescribeDto extends TagResultDto implements ITag {
+  @Expose()
+  @IsEnum(TagTypes)
+  type: TagTypes;
+
+  @Expose()
+  @IsString()
+  name: string;
+
+  @Expose()
+  @IsString()
+  describe: string;
+
+  using: boolean;
 }
