a4d986f8919f224863dc5aaf74beda48f14197f6
Roharui <jack555586@gmail.com>
2022-09-30 17:13:21 +0900

    fix: 토큰없이 유저 데코레이터 호출시 발생하는 불필요한 로직 수정

--- src/common/decorator/user.decorator.ts
+++ src/common/decorator/user.decorator.ts
@@ -7,6 +7,8 @@ export const User = createParamDecorator(
   async (data: unknown, ctx: ExecutionContext) => {
     const { authorization } = ctx.switchToHttp().getRequest<Request>().headers;
 
+    if (authorization == null) return null;
+
     //TODO: 후일 localhost에서 인증서버의 주소로 변경할 것.
     return await api<IProfile>('http://localhost:8080/profile', {
       headers: { authorization },
