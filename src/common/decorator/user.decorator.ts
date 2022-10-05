990594120aefd5665648c2d3a12053c1ea17d85a
Roharui <jack555586@gmail.com>
2022-10-05 13:10:33 +0900

    refactor: 불필요한 async await 제거

--- src/common/decorator/user.decorator.ts
+++ src/common/decorator/user.decorator.ts
@@ -4,13 +4,13 @@ import { IProfile } from '@src/dto/ProfileDto';
 import { api } from '../utils/api';
 
 export const User = createParamDecorator(
-  async (data: unknown, ctx: ExecutionContext): Promise<IProfile | null> => {
+  (data: unknown, ctx: ExecutionContext): Promise<IProfile | null> => {
     const { authorization } = ctx.switchToHttp().getRequest<Request>().headers;
 
     if (authorization == null) return null;
 
     //TODO: 후일 localhost에서 인증서버의 주소로 변경할 것.
-    return await api<IProfile>('http://localhost:8080/profile', {
+    return api<IProfile>('http://localhost:8080/profile', {
       headers: { authorization },
     }).catch(() => null);
   },
