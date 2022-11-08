ecef0bf75f72627f3dce149a7efe93980dfa380b
Roharui <jack555586@gmail.com>
2022-11-08 13:34:12 +0900

    fix: AWS s3의 수정사항 반영
    프로필 이미지 리사이징 기능때문에 origin을 경로에 추가하였습니다.

--- src/rabbitmq/rabbit.service.ts
+++ src/rabbitmq/rabbit.service.ts
@@ -20,7 +20,7 @@ export class RabbitService {
       await s3.send(
         new CopyObjectCommand({
           Bucket: AWS_S3_BUCKET_NAME,
-          Key: 'profile/' + realKey,
+          Key: 'profile/origin/' + realKey,
           CopySource: AWS_S3_BUCKET_NAME + '/temp/' + realKey,
         }),
       );
@@ -31,7 +31,7 @@ export class RabbitService {
         }),
       );
 
-      return { status: 200, key: 'profile/' + realKey };
+      return { status: 200, key: 'profile/origin/' + realKey };
     } catch (e: unknown) {
       console.error(e);
       let message = null;
